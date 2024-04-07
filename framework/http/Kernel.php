84d2004ed23b0daf147fdfabcdbb964adecc5631
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2024-04-07 18:27:53 +0200

    Deleting unused clases

--- framework/http/Kernel.php
+++ framework/http/Kernel.php
@@ -4,17 +4,14 @@ namespace Cfw\Http;
 
 use Cfw\Router\Router;
 use Cfw\Http\Request;
-use Cfw\Http\Response;
 
 class Kernel
 {
     private Request $request;
-    private Response $response;
 
     public function __construct()
     {
         $this->request = Request::createFromGlobals();
-        $this->response = new Response();
     }
 
     public function handle(Request $request): void
